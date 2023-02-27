2cb8106d098d799e540e9d3bdcdbaa8ba1e83454
pokidyuk <pokidyuk@MR012>
2023-02-27 10:54:17 +0300

    Add project files.

---
+++ BinarySearch/BinarySearch.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+#include <iostream>
+using namespace std;
+
+void search(int* mas, int b, int e, int x, int* low, int* high) {
+    if (b > e) return;
+    int mid = (e + b) / 2;
+    if (mas[mid] > x) search(mas, b, mid - 1, x, low, high);
+    else if (mas[mid] < x) search(mas, mid + 1, e, x, low, high);
+    else {
+        int l = mid;
+        int h = mid;
+        while (l > b && mas[l - 1] == x) l--;
+        while (h < e && mas[h + 1] == x) h++;
+        *low = l;
+        *high = h;
+    }
+}
+
+int main()
+{
+    int m[] = { 3, 4, 4, 4, 5, 6, 7, 7, 8, 9 };
+    int low, high;
+    low = -1,
+        high = -1;
+    search(m, 0, 9, 5, &low, &high);
+    // Если low и  high остались -1 - то элемент не найден.
+    cout << low << " " << high;
+    return 0;
+}
---
+++ Function/LineSearch.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "rus");
+
+    int ans[20]; // создали массив для записи всех индексов
+    int h = 0;
+    int arr[20]; // создали массив на 20 элементов
+    int key; // создали переменную в которой будет находиться ключ
+
+    srand(time(NULL));
+
+    for (int i = 0; i < 20; i++) {
+        arr[i] = 1 + rand() % 20; // заполняем случайными числами ячейки
+
+        cout << arr[i] << " "; // выводим все ячейки массива
+
+        if (i == 9) {
+            cout << endl;
+        }
+    }
+
+    cout << endl << endl << "Введите ключ : "; cin >> key; // считываем ключ
+
+    for (int i = 0; i < 20; i++) {
+        if (arr[i] == key) { // проверяем равен ли arr[i] ключу
+            ans[h++] = i;
+        }
+    }
+
+    if (h != 0) { // проверяем были ли совпадения
+        for (int i = 0; i < h; i++) {
+            cout << "Ключ " << key << " находится в ячейке " << ans[i] << endl; //выводим все индексы
+        }
+    }
+    else {
+        cout << "Мы не нашли ключ " << key << " в массиве";
+    }
+
+    return 0;
+}
